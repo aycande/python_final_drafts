09a253273e5e4a2a5fabfd2d470fed7ed8ff7374
Deniz Aycan <aycan@uchicago.edu>
2023-12-06 19:58:54 -0600

    Update draft3.py

--- draft3.py
+++ draft3.py
@@ -7,3 +7,24 @@ Created on Wed Dec  6 10:45:27 2023
 """
 
 #exactly as draft2 but to make sure we copied all parts to seperate pys
+
+
+#Part 2: PDF Parsing
+
+
+
+
+
+#Part 5: Plot 1
+
+#Part 4: Shiny
+#Case Preparation for South Africa and India
+#for oecd, create a function where you are able to choose which variable you would like to plot over time and for which country using this code as basis:
+
+
+panel3.columns
+
+OECD_excel_file_path = os.path.join(base_path, 'OECD.xlsx')
+OECD.to_excel(OECD_excel_file_path, index=False)
+PANEL_excel_file_path = os.path.join(base_path, 'panel3.xlsx')
+panel3.to_excel(PANEL_excel_file_path, index=False)
